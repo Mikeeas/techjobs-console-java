b7cffd1932888e02e66a7d8b3c121586e6aa67d2
michaeleisenmayer <mikeeisenmayer@yahoo.com>
2019-08-19 13:41:10 -0500

    Commiting Changes made to print jobs

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -117,6 +117,7 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
+
         for (HashMap<String, String> jobs : JobData.findAll()){
             for (String key : jobs.keySet()) {
                 String value = jobs.get(key);
